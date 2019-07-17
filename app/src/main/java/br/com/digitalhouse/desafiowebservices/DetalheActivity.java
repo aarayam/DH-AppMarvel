7179a1c17c9d90d211cd1d1a9b1c93a0a6096b03
mayarals <limas.mayara@gmail.com>
2019-07-16 22:35:36 -0300

    tela splash

---
+++ app/src/main/java/br/com/digitalhouse/desafiowebservices/DetalheActivity.java
@@ -0,0 +1,14 @@
+package br.com.digitalhouse.desafiowebservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comics_detalhe);
+    }
+}
